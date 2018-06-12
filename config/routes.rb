ee9c71a8b8cce79b0021d06ac1101d4ee7af67fc
Andrewx987 <andres.pb.16@gmail.com>
2018-06-12 16:52:43 -0400

    Primer commit proyecto angular

---
+++ config/routes.rb
@@ -0,0 +1,44 @@
+Rails.application.routes.draw do
+  resources :teachers do
+    resources :courses do
+      resources :students do
+        resources :c_students do
+          resources :homeworks
+        end    
+      end
+    end
+  end
+
+  resources :teachers do
+    resources :courses do
+      resources :students do
+        resources :c_students do
+          resources :tests
+        end    
+      end
+    end
+  end
+
+  resources :students do
+    resources :calendar
+  end
+
+  resources :teachers do
+    resources :courses do
+      resources :classrooms do
+        resources :audios
+      end
+    end
+  end
+
+  resources :teachers do
+    resources :courses do
+      resources :classrooms do
+        resources :videos
+      end
+    end
+  end
+
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
