28685d72bf46e20c54e01b8c26b8cd393149ee0c
Andrewx987 <andres.pb.16@gmail.com>
2018-06-13 17:50:06 -0400

    Comente el segundo get de teachers

--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -6,7 +6,7 @@ class TeachersController < ApplicationController
     #  @teachers = Teacher.all
     # json_response(@teachers)
     #end
-  
+
     #POST /teachers
     #def create
 
@@ -15,28 +15,28 @@ class TeachersController < ApplicationController
     #end
 
     # GET /teachers/:teacher_id
-    def show
-      json_response(@teacher)
-    end
-  
+    # def show
+    #   json_response(@teacher)
+    # end
+    #
     # PUT /teacher/:teacher_id
     def update
       @teacher.update(teacher_params)
       head :no_content
     end
-  
+
     # DELETE /teachers/:teacher_id
     def destroy
       @teacher.destroy
       head :no_content
     end
-  
+
     private
-  
+
     def teacher_params
       params.permit(:nombre, :correo, :edad, :rut, :especialidad)
     end
-  
+
     def set_teacher
       @teacher = Teacher.find(params[:id])
     end
